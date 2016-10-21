30d9be1696e0ff7637ce9398319d43ba415e64d7
olesyaizlesa <olesyaizlesa1@gmail.com>
2016-10-21 14:40:23 +0300

    Add files via upload

---
+++ app/js/js/functions.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+    function setPosition(img) {
+      var positionX = getRandomInt(477, 820);
+      var positionY = getRandomInt(0, 340);
+
+    img.css({
+      "top": positionY,
+      "left": positionX
+    });
+  }
+  
+  var mas = [];
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      var id = "#" + i + "_" + j;
+      mas.push($(id));
+    }
+  }
+
+  for (var i = 0; i < 9; i++) {
+    setPosition(mas[i]);
+  }
+
+});
