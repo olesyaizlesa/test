0484bcd9fbc4ac6b15eaff4c0e99153cfef7fcd5
olesyaizlesa <olesyaizlesa1@gmail.com>
2016-10-25 09:33:54 +0300

    Update functions.js
    Убрала $(document).ready(function() {});
    Настроила отступы

--- app/js/functions.js
+++ app/js/functions.js
@@ -1,15 +1,13 @@
-$(document).ready(function() {});
-
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-    function setPosition(img) {
-      var positionX = getRandomInt(477, 820);
-      var positionY = getRandomInt(0, 340);
+function setPosition(img) {
+  var positionX = getRandomInt(477, 820);
+  var positionY = getRandomInt(0, 340);
 
-    img.css({
-      "top": positionY,
-      "left": positionX
-    });
-  }
+  img.css({
+    "top": positionY,
+    "left": positionX
+  });
+}
