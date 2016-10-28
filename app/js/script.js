3ea29e2db3c609a6bea49229834db995694b3fcf
olesyaizlesa <olesyaizlesa1@gmail.com>
2016-10-28 12:01:20 +0300

    Кусочки пазла отскакивают от занятых клеток

--- app/js/script.js
+++ app/js/script.js
@@ -1,20 +1,15 @@
-$(document).ready(function() {
+	$(document).ready(function() {
 
-		$(".test").droppable();
-	
-		$(".image").draggable({ containment:".action", 
-								snap: ".test-inner",
-								snapMode: "inner",
-								revert: "invalid",
-		start: function(){
-	    console.log("START");
-        },
-        drag: function(event, ui){
-	    console.log("DRAG");
-        },
-        stop: function(){
-	    console.log("STOP");
-        }
+		$(".test-inner").droppable( { tolerance: "fit",
+		out: function() { $(this).droppable("enable"); },
+		drop: function() { $(this).droppable("disable"); }
+	});
+
+		$(".image").draggable( { containment:".action",	snap: ".test-inner",
+		snapMode: "inner", revert: "invalid",
+	  stack: ".image",
+		start: function() { console.log("start"); },
+		drag: function(event, ui) { console.log("drag"); },
+		stop: function(event, ui) { console.log("ui.position");	}
 		});
-	
-});
+	});
